e80bd49108d6ae40002851fdbeb53fd6b0c15c2b
Muhammad Musharib <112599582+Musharibkabir@users.noreply.github.com>
2022-12-05 10:47:08 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const express = require('express')
+const app = express()
+const port = 
+const path = require('path')
+
+
+
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+app.post('/comment', (req, res) => {
+    console.log(req.body)  
+    res.json(req.body)
+})
+
+
+app.get('/', (req, res) => {
+     res.sendFile('Post request Recievedl')
+})    
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, './public',  'index.html'))
+
+})
+
+app.get('/Musharibkabir', (req, res) => {
+    res.sendFile(path.join(__dirname, './public',  'Musharibkabir.html'))
+})
